84b0988e0f60d8906b733b655ec7147d0fab185c
Ky <ky.truong@digital.justice.gov.uk>
2025-05-29 23:43:30 +0100

    outputs

---
+++ outputs.tf
@@ -0,0 +1,30 @@
+output "cloudwatch_log_group_name" {
+  value = aws_cloudwatch_log_group.firehose.name
+}
+
+output "data_stream" {
+  value = aws_kinesis_firehose_delivery_stream.firehose.id
+}
+
+output "firehose_server_side_encryption_key_arn" {
+  value = aws_kinesis_firehose_delivery_stream.firehose.server_side_encryption[0].key_arn
+}
+
+output "iam_roles" {
+  value = {
+    "cloudwatch-to-firehose" = aws_iam_role.cloudwatch-to-firehose,
+    "firehose-to-s3"         = aws_iam_role.firehose
+  }
+}
+
+output "kms_key_arn" {
+  value = aws_kms_key.firehose.arn
+}
+
+output "log_subscriptions" {
+  value = aws_cloudwatch_log_subscription_filter.cloudwatch-to-firehose
+}
+
+output "secretsmanager_secret_arn" {
+  value = aws_secretsmanager_secret.firehose.arn
+}
