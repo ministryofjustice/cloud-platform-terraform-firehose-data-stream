c46d0790e495e441e19440a8db772a1b5a888f62
folarin oyenuga <folarin.oyenuga@digital.justice.gov.uk>
2026-03-31 12:25:07 +0100

    fix: correct example module interface to use tags map

--- examples/main.tf
+++ examples/main.tf
@@ -2,16 +2,23 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-module "template" {
+module "firehose_data_stream" {
   source = "../"
-  # source = "github.com/ministryofjustice/cloud-platform-terraform-template?ref=version" # use the latest release
+  # source = "github.com/ministryofjustice/cloud-platform-terraform-firehose-data-stream?ref=version" # use the latest release
+
+  # Configuration
+  cloudwatch_log_group_names = []
+  destination_http_endpoint  = ""
+  destination_bucket_arn     = ""
 
   # Tags
-  business_unit          = var.business_unit
-  application            = var.application
-  is_production          = var.is_production
-  team_name              = var.team_name
-  namespace              = var.namespace
-  environment_name       = var.environment_name
-  infrastructure_support = var.infrastructure_support
+  tags = {
+    business_unit          = var.business_unit
+    application            = var.application
+    is_production          = var.is_production
+    team_name              = var.team_name
+    namespace              = var.namespace
+    environment_name       = var.environment_name
+    infrastructure_support = var.infrastructure_support
+  }
 }
